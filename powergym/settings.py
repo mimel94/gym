564078800eb3e2e5725b907fc0aab4e103b539e0
Stephania Zambrano <stezague88@gmail.com>
2021-08-29 20:32:04 -0500

    Diseño hover input

--- powergym/settings.py
+++ powergym/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'powergym',
         'USER': 'postgres',
-        'PASSWORD': 'abcd.1234',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': 5432
     }
